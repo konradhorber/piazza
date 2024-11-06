82e3db58e0051628c0763a957b3b24710259b1a3
konradhorber <konrad.horber@gmail.com>
2024-11-06 14:11:36 +0000

    Add mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 
 const app = express();
 
 app.get("/", (req,res) =>{
-    res.send("hello app running");
+    res.send("test app running");
+});
+
+mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
+    console.log("DB is connected");
 });
 
 app.listen(80);
