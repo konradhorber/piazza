e8558d53efe9443a0f0a34cd50d68b9eeb40b24d
konradhorber <konrad.horber@gmail.com>
2024-11-08 18:53:36 +0000

    Refactor register validation and comments

--- validations/register-validation.js
+++ validations/register-validation.js
@@ -1,8 +1,9 @@
 // import package for validation
 const joi = require("joi");
 
-// set register validation schema incl trim for whitespaces
+// set register validation 
 const registerValidation = (data) => {
+    // define joi schema
     const schemaValidation = joi.object({
         username: joi.string()
             .trim()
@@ -18,12 +19,14 @@ const registerValidation = (data) => {
             .email(),
         
         password: joi.string()
+            .trim()    
             .required()
             .min(6)
             .max(1024)
     });
+    // validate data against schema rules
     return schemaValidation.validate(data);
 };
 
-// export register validation schema to app
+// export register validation to app
 module.exports.registerValidation = registerValidation;
